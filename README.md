11fc3987f091b47dd5f55b9db1a4e368c3504e78
James Laver <james.laver@gmail.com>
2020-08-04 12:45:00 +0200

    Initial commit, seems to work

---
+++ README.md
@@ -0,0 +1,15 @@
+# ping-pong-cell
+
+<!-- [![License](https://img.shields.io/crates/l/ping-pong-cell.svg)](https://github.com/irrustible/ping-pong-cell/blob/main/LICENSE) -->
+<!-- [![Package](https://img.shields.io/crates/v/ping-pong-cell.svg)](https://crates.io/crates/ping-pong-cell) -->
+<!-- [![Documentation](https://docs.rs/ping-pong-cell/badge.svg)](https://docs.rs/ping-pong-cell) -->
+
+An atomic cell game for up to two players.
+
+## Copyright and License
+
+Copyright (c) 2020 James Laver, ping-pong-cell Contributors
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
---
+++ src/lib.rs
@@ -0,0 +1,127 @@
+#![no_std]
+use core::cell::UnsafeCell;
+use core::sync::atomic::{AtomicUsize, spin_loop_hint};
+use core::sync::atomic::Ordering::{Acquire, Release};
+
+/// An Atomic Cell game for up to two players.
+#[derive(Debug)]
+pub struct PingPongCell<T> {
+    state: AtomicUsize,
+    value: UnsafeCell<Option<T>>,
+}
+
+const WAITING: usize = 0;
+const WORKING: usize = 0b01;
+
+impl<T: Send> PingPongCell<T> {
+
+    /// Create a new PingPongCell
+    pub fn new(value: Option<T>) -> Self {
+        PingPongCell {
+            state: AtomicUsize::new(WAITING),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    /// If there is a value currently inside, take it.
+    pub fn take(&self) -> Option<T> {
+        loop {
+            if let WAITING = self.state.compare_and_swap(WAITING, WORKING, Acquire) {
+                return unsafe {
+                    let val = (*self.value.get()).take();
+                    self.state.store(WORKING, Release);
+                    val
+                }
+            } else {
+                spin_loop_hint();
+            }
+        }
+    }
+
+    /// Replace the value inside unconditionally.
+    pub fn put(&self, value: T) {
+        loop {
+            if let WAITING = self.state.compare_and_swap(WAITING, WORKING, Acquire) {
+                unsafe {
+                    *self.value.get() = Some(value);
+                    self.state.store(WAITING, Release);
+                    return
+                }
+            } else {
+                spin_loop_hint();
+            }
+        }
+    }
+
+    /// Puts a value inside only if there is not one currently.
+    pub fn put_if_empty(&self, value: T) -> Result<(), T> {
+        loop {
+            if let WAITING = self.state.compare_and_swap(WAITING, WORKING, Acquire) {
+                return unsafe {
+                    let inner = self.value.get();
+                    if (*inner).is_some() {
+                        Err(value)
+                    } else {
+                        *inner = Some(value);
+                        self.state.store(WAITING, Release);
+                        Ok(())
+                    }
+                }
+            } else {
+                spin_loop_hint();
+            }
+        }
+    }
+}
+
+impl<T: Clone + Send> PingPongCell<T> {
+    /// Clones the contents, if any.
+    pub fn clone_inner(&self) -> Option<T> {
+        loop {
+            if let WAITING = self.state.compare_and_swap(WAITING, WORKING, Acquire) {
+                return unsafe {
+                    let val = (*self.value.get()).clone();
+                    self.state.store(WAITING, Release);
+                    val
+                }
+            } else {
+                spin_loop_hint();
+            }
+        }
+    }
+}
+
+impl<T: Eq + Send> PingPongCell<T> {
+    /// A single CAS operation
+    pub fn compare_and_swap(&self, expected: &T, new: T) -> Result<(), T> {
+        loop {
+            if let WAITING = self.state.compare_and_swap(WAITING, WORKING, Acquire) {
+                return unsafe {
+                    let inner = self.value.get();
+                    if let Some(val) = &*inner {
+                        let ret = if val == expected {
+                            *inner = Some(new);
+                            Ok(())
+                        } else {
+                            Err(new)
+                        };
+                        self.state.store(WAITING, Release);
+                        ret
+                    } else {
+                        self.state.store(WAITING, Release);
+                        Err(new)
+                    }
+                }
+            } else {
+                spin_loop_hint();
+            }
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    // #[test]
+    // fn it_works() {
+    //     assert_eq!(2 + 2, 4);
+    // }
+}
